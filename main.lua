596675754ae179ed971ed454be639c360d274e94
b99517747-cell <b99517747@gmail.com>
2026-06-10 01:00:34 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -485,7 +485,7 @@ MainTab:Image({
 MainTab:Space()
 
 -- SEKCJA: Statystyki (tworzona raz, nigdy nie niszczona)
-local StatSekcja = MainTab:Section({ Title = "Statystyki", Icon = "bar-chart-2", Opened = true })
+local StatSekcja = MainTab:Section({ Title = "Statystyki", Icon = "pie-chart", Opened = true })
 
 statBazaEl = StatSekcja:Paragraph({
     Title = "Kont w bazie konfidentów: 0",
